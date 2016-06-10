8079d23f1dfd84e45882991d412ee73e7b65bc34
Kacper <Ryuu4@o2.pl>
2016-06-10 18:55:51 +0200

    Obsługa przycisków

--- app/src/main/java/com/example/kacper_light_erp/bestcalendareu/BestCalendarEU.java
+++ app/src/main/java/com/example/kacper_light_erp/bestcalendareu/BestCalendarEU.java
@@ -1,6 +1,8 @@
 package com.example.kacper_light_erp.bestcalendareu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,13 +10,52 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.CalendarView;
 
 public class BestCalendarEU extends AppCompatActivity {
+    Handler handler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_best_calendar_eu);
+        handler = new Handler();
+        Button dodaj = (Button) findViewById(R.id.newevent);
+
+        dodaj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(getApplicationContext(), AddEventActivity.class);
+                        startActivity(intent);
+                    }
+                }, 250);
+            }
+        });
+        Button lista = (Button) findViewById(R.id.list);
+        lista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(getApplicationContext(), CalendarTaskList.class);
+                        startActivity(intent);
+                    }
+                }, 250);
+            }
+        });
+        CalendarView cv= (CalendarView) findViewById(R.id.calendarView);
+        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                
+            }
+        });
     }
 
     @Override
