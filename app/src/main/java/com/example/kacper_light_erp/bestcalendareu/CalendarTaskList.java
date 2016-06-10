54f556a9386731b411356b9b72677d1308c9a748
Kacper <Ryuu4@o2.pl>
2016-06-10 18:12:13 +0200

    1st commit

---
+++ app/src/main/java/com/example/kacper_light_erp/bestcalendareu/CalendarTaskList.java
@@ -0,0 +1,18 @@
+package com.example.kacper_light_erp.bestcalendareu;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CalendarTaskList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar_task_list);
+    }
+
+}
